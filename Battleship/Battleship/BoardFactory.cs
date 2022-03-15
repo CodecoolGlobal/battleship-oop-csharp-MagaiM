7a7590205c57dd505832307e28fcbda6877a5c81
galandras13 <galandras13@gmail.com>
2022-03-15 16:29:25 +0100

    Add skeleton for ManualPlacement method

--- Battleship/Battleship/BoardFactory.cs
+++ Battleship/Battleship/BoardFactory.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Codecool.Battleship.UI;
 
 namespace Codecool.Battleship
 {
     internal class BoardFactory
     {
+        public void manualPlacement(Board board, Player player)
+        {
+            //int x, int y, Direction direction, ShipType shipType
+            var shipTypes = Enum.GetValues(typeof(ShipType));
+
+            foreach (var shipType in shipTypes)
+            {
+                
+                while (true)
+                {
+
+                }
+            }
+            //Ship ship = new Ship(x, y, direction, shipType);
+            //display.PrintLine("Give a cord to put your ship");
+            //(int x, int y) shipStartingCords = input.GetCords();
+        }
     }
 }
