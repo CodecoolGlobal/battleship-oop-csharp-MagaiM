8c41aa06b82ce5e88aa86f4ab3eb0a33825e8be1
galandras13 <galandras13@gmail.com>
2022-03-15 16:25:05 +0100

    Implement methods that handle the UI

--- Battleship/Battleship/UI/Display.cs
+++ Battleship/Battleship/UI/Display.cs
@@ -1,6 +1,32 @@
-﻿namespace Codecool.Battleship.UI
+﻿using System.Text;
+
+namespace Codecool.Battleship.UI
 {
     internal class Display
     {
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void ShowMenu(List<string> options)
+        {
+            Console.Clear();
+            //Console.OutputEncoding = Encoding.UTF8;
+            for (int i = 1; i < options.Count; i++)
+            {
+                Console.WriteLine($"({i}). {options[i]}");
+            }
+            
+            Console.WriteLine($"(0). {options[0]}" + Environment.NewLine);
+        }
+
+        public void PrintBoard()
+        {
+        }
+
+        public void PrintResult()
+        {
+        }
     }
 }
--- Battleship/Battleship/UI/Input.cs
+++ Battleship/Battleship/UI/Input.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Codecool.Battleship.UI
 {
     internal class Input
     {
+        public (int, int) GetCords()
+        {
+            int x;
+            int y;
+            string shipStartingCords = "";
+            while (!IsValidCordFormat(shipStartingCords))
+            {
+                shipStartingCords = Console.ReadLine();
+            }
+
+            x = ConvertLetterToNum(shipStartingCords[0]);
+            y = Convert.ToInt32(shipStartingCords.Substring(1));
+
+            return (x, y);
+        }
+
+        private int ConvertLetterToNum(char row)
+        {
+            return (int)Char.ToLower(row) - (int)'a';
+        }
+
+        private bool IsValidCordFormat(string cords)
+        {
+
+            return Regex.IsMatch(cords, @"^[a-zA-Z]{1}\d+$");
+        }
+
+        public int SelectMenu(List<string> options)
+        {
+            while (true)
+            {
+                try
+                {
+                    int input = Convert.ToInt32(Console.ReadLine());
+                    if (input >= 0 && input < options.Count)
+                        return input;
+                }
+                catch (Exception e)
+                {
+                }
+            }
+        }
     }
 }
