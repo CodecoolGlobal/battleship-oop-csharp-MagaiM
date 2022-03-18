42f7af2ab07b62bcd05711ecd8f179adc15540ad
MagaiM <magai.marton@gmail.com>
2022-03-18 11:41:43 +0100

    Refactor BoardFactory

--- Battleship/Battleship/BoardFactory.cs
+++ Battleship/Battleship/BoardFactory.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Codecool.Battleship.Enums;
+using Codecool.Battleship.Players;
 using Codecool.Battleship.UI;
 
 namespace Codecool.Battleship
