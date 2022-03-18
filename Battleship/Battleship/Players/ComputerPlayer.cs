499f2d890f1a92a67cf8247884ceb4a24db4de58
MagaiM <magai.marton@gmail.com>
2022-03-18 11:35:54 +0100

    Refactor ComputerPlayer

---
+++ Battleship/Battleship/Players/ComputerPlayer.cs
@@ -0,0 +1,12 @@
+﻿using Codecool.Battleship.UI;
+
+namespace Codecool.Battleship.Players
+{
+    internal class ComputerPlayer : Player
+    {
+
+        public ComputerPlayer(Display display) : base(display)
+        {
+        }
+    }
+}
