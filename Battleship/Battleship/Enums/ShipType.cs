514e0aa2c6c7bb0db67e83cd8e881246f16d6c75
MagaiM <magai.marton@gmail.com>
2022-03-18 11:40:38 +0100

    Refactor ShipTypes

---
+++ Battleship/Battleship/Enums/ShipType.cs
@@ -0,0 +1,11 @@
+﻿namespace Codecool.Battleship.Enums
+{
+    enum ShipType
+    {
+        Carrier = 5,
+        Battleship = 4,
+        Cruiser = 3,
+        Submarine = 3,
+        Destroyer = 2
+    }
+}
